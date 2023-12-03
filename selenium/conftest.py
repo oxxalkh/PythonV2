4936a0bf4a2e65776358b35cf132eb556a4df82f
oxanaalkh <ksenikrsk@mail.ru>
2023-12-03 22:13:29 +0300

    hw2

---
+++ selenium/conftest.py
@@ -0,0 +1,73 @@
+import pytest
+import yaml
+from module import Site
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+
+@pytest.fixture()
+def selector_log():
+    return '//*[@id="login"]/div[1]/label/input'
+
+
+@pytest.fixture()
+def selector_pass():
+    return '//*[@id="login"]/div[2]/label/input'
+
+
+@pytest.fixture()
+def selector_button():
+    return '//*[@id="login"]/div[3]'
+
+
+@pytest.fixture()
+def selector_err():
+    return '//*[@id="app"]/main/div/div/div[2]/h2'
+
+
+@pytest.fixture()
+def res_authorisation():
+    return '//*[@id="app"]/main/nav/a'
+
+
+
+@pytest.fixture()
+def new_post():
+    return '//*[@id="create-btn"]'
+
+
+@pytest.fixture()
+def input_title():
+    return '//*[@id="create-item"]/div/div/div[1]/div/label/input'
+
+
+@pytest.fixture()
+def input_description():
+    return '//*[@id="create-item"]/div/div/div[2]/div/label/span/textarea'
+
+
+@pytest.fixture()
+def input_content():
+    return '//*[@id="create-item"]/div/div/div[3]/div/label/span/textarea'
+
+@pytest.fixture()
+def selector_button2():
+    return '//*[@id="create-item"]/div/div/div[7]/div/button'
+
+
+@pytest.fixture()
+def post_title():
+    return '//*[@id="app"]/main/div/div[1]/h1'
+
+
+
+@pytest.fixture()
+def site():
+    my_site = Site(testdata["address"])
+    yield my_site
+    my_site.close()
+
+
+
+
---
+++ selenium/test_1.py
@@ -0,0 +1,57 @@
+import yaml
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+
+def test_step1(site, selector_log, selector_pass,
+               selector_button, selector_err):
+
+    input1 = site.find_element("xpath", selector_log)
+    input1.send_keys("test")
+    input2 = site.find_element("xpath", selector_pass)
+    input2.send_keys("test")
+    btn = site.find_element("xpath", selector_button)
+    btn.click()
+    err_label = site.find_element("xpath", selector_err)
+    assert err_label.text == "401"
+
+
+def test_step2(site, selector_log, selector_pass,
+               selector_button, res_authorisation):
+    input1 = site.find_element("xpath", selector_log)
+    input1.send_keys(testdata["login"])
+    input2 = site.find_element("xpath", selector_pass)
+    input2.send_keys(testdata["password"])
+    btn = site.find_element("xpath", selector_button)
+    btn.click()
+    authorisation = site.find_element("xpath", res_authorisation)
+    assert authorisation.text == "Home"
+
+
+def test_step3(site, selector_log, selector_pass,
+               selector_button, new_post, input_title, input_description,
+               input_content, selector_button2, post_title):
+    input1 = site.find_element("xpath", selector_log)
+    input1.send_keys(testdata["login"])
+    input2 = site.find_element("xpath", selector_pass)
+    input2.send_keys(testdata["password"])
+    btn = site.find_element("xpath", selector_button)
+    btn.click()
+    post = site.find_element("xpath", new_post)
+    post.click()
+    input3 = site.find_element("xpath", input_title)
+    input3.send_keys(testdata["title"])
+    input4 = site.find_element("xpath", input_description)
+    input4.send_keys(testdata["description"])
+    input5 = site.find_element("xpath", input_content)
+    input5.send_keys(testdata["content"])
+    btn2 = site.find_element("xpath", selector_button2)
+    btn2.click()
+    p_title = site.find_element("xpath", post_title)
+    assert p_title.text == testdata["title"]
+
+
+
+
+
